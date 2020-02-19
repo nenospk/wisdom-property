a08622a4fd2cf27cdfc91e57c45947ba636d8c8b
Suppakit Neno <suppakit.neno@gmail.com>
2020-02-19 21:17:50 +0700

    floor plan

--- js/app.js
+++ js/app.js
@@ -5,3 +5,10 @@ $(document).ready(function(){
 		$(".logo_nav").toggle();
 	});
 });
+
+function display_floor_plan(floor) {
+	let img_url = "img/plan_floor_" + floor + ".jpg"
+	$(".floor-display").html('<img src="' + img_url + '" width="300">');
+	$(".floor-each").removeClass("floor-selected");
+	$(".floor-" + floor).addClass("floor-selected");
+}
