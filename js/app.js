5bc3d576c006a19319d0b6f49d89f39906588ae0
Suppakit Neno <suppakit.neno@gmail.com>
2020-03-25 19:37:15 +0700

    image name

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-	$(".nav-button").click(function(){
+$(document).ready(function() {
+	$(".nav-button").click(function() {
 		//$(".logo_nav").css("display", "block");
 		$(".logo_bg_2").toggle();
 		$(".logo_nav").toggle();
@@ -7,29 +7,36 @@ $(document).ready(function(){
 		$(".logo_nav").removeClass("slow");
 	});
 
-	$('a[href^="#"]').on('click', function(event) {
-		var hash = '#' + $(this).attr('href').split('#')[1]
-		var element = $(hash)
+	$('a[href^="#"]').on("click", function(event) {
+		var hash =
+			"#" +
+			$(this)
+				.attr("href")
+				.split("#")[1];
+		var element = $(hash);
 		if (element.length) {
-		  event.preventDefault();
-		  history.pushState(hash, undefined, hash)
-		  $('html, body').animate({scrollTop: element.offset().top}, 1000)
-		}
-	});   
-
-	window.addEventListener('popstate', function(e) {
-		if(e.state && e.state.startsWith('#') && $(e.state).length){
-		  $('html, body').animate({scrollTop: $(e.state).offset().top}, 1000)
+			event.preventDefault();
+			history.pushState(hash, undefined, hash);
+			$("html, body").animate({ scrollTop: element.offset().top }, 1000);
 		}
 	});
 
-	$('html, body').on("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove", function(){
-		$('html, body').stop();
+	window.addEventListener("popstate", function(e) {
+		if (e.state && e.state.startsWith("#") && $(e.state).length) {
+			$("html, body").animate({ scrollTop: $(e.state).offset().top }, 1000);
+		}
 	});
+
+	$("html, body").on(
+		"scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove",
+		function() {
+			$("html, body").stop();
+		}
+	);
 });
 
 function display_floor_plan(floor) {
-	let img_url = "img/plan_floor_" + floor + ".jpg"
+	let img_url = "img/plan-floor-" + floor + ".jpg";
 	$(".floor-display").html('<img src="' + img_url + '" width="300">');
 	$(".floor-each").removeClass("floor-selected");
 	$(".floor-" + floor).addClass("floor-selected");
